d5c64e916e8541a2cb90b1584be377296a7e92bb
Riley PC <riley-ball@outlook.com>
2018-12-07 00:21:25 +1000

    fin

---
+++ src/star1.py
@@ -0,0 +1,9 @@
+def main():
+    count = 0
+    with open ("./input.txt", "r") as input:
+        for line in input:
+            count += int(line)
+    print(count)
+
+if __name__ == "__main__":
+    main()
