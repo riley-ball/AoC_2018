94b05cf19652a484c2d74b385d151729e8ae12ca
Riley PC <riley-ball@outlook.com>
2018-12-07 00:18:45 +1000

    day 1, star 2

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
 def main():
     count = 0
-    with open ("./input.txt", "r") as input:
-        for line in input:
+    number_list = [0]
+    flag = 0
+    list_count = 0
+    while flag == 0:
+        with open ("./input.txt", "r") as input:
+            list_count += 1
+            print(list_count)
+            for line in input:
                 count += int(line)
-    print(count)
+                if count in number_list:
+                    print(count)
+                    flag = 1
+                    break
+                number_list.append(count)
 
 if __name__ == "__main__":
     main()
